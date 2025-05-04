504c03f114cd820e6a7197eb6a219a69ae338187
youngho9999 <aplblypt@gmail.com>
2025-05-05 05:29:13 +0900

    [feat] 생성자 주입을 위해 SpringContextInitiator 로직 변경. 먼저 생성자 필요없는 인스턴스 생성. 그 이후 위상 정렬 이용해서 그래프 탐색하면서 인스턴스 생성.

--- src/main/java/live/context/SpringContextInitiator.java
+++ src/main/java/live/context/SpringContextInitiator.java
@@ -2,28 +2,144 @@ package live.context;
 
 import live.exception.ContextInitializeException;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 public class SpringContextInitiator {
 
-    public void createInstances(Set<Class<?>> scannedComponents) {
+    private static Map<Class<?>, Integer> clazzInDegree;
+    private static Map<Class<?>, Set<Class<?>>> clazzGraph;
+
+    public void init(Set<Class<?>> scannedComponents) {
+        createInstancesForNoConstructorInjectionRequired(scannedComponents);
+        injectToFields();
+        injectBySetter();
+        Queue<Class<?>> topologyQueue = initializeInDegreeForConstructor(scannedComponents);
+        int initializedInstances = injectConstructorWithTopologySort(topologyQueue);
+
+        if(initializedInstances != scannedComponents.size()) {
+            throw new ContextInitializeException("cycle detected: " + scannedComponents.size() + " components were initialized");
+        }
+    }
+
+    public void createInstancesForNoConstructorInjectionRequired(Set<Class<?>> scannedComponents) {
+        Set<Class<?>> toRemove = new HashSet<>();
+
+        clazzloop:
         for (Class<?> clazz : scannedComponents) {
+            //create only for instance without constructor injection
+            for (Constructor<?> constructor : clazz.getConstructors()) {
+                if (constructor.isAnnotationPresent(AutoWire.class)) {
+                    continue clazzloop;
+                }
+            }
+
             try {
+                clazz.getDeclaredConstructor().newInstance();
                 Object o = clazz.getDeclaredConstructor().newInstance();
                 SpringContext.BEAN_MAP.put(clazz, o);
+                toRemove.add(clazz);
             } catch (InstantiationException | InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
                 throw new ContextInitializeException(e);
             }
         }
+
+        scannedComponents.removeAll(toRemove);
     }
 
-    public void injectDependencies() {
-        injectToFields();
-        injectBySetter();
+    public Queue<Class<?>> initializeInDegreeForConstructor(Set<Class<?>> scannedComponents) {
+        //진입차수 저장 Map
+        clazzInDegree = new HashMap<>();
+        //그래프 탐색을 위한 Map
+        clazzGraph = new HashMap<>();
+
+        //initialize in degree for all classes
+        for (Class<?> clazz : scannedComponents) {
+            initializeEachClassForTopologySort(clazz);
+        }
+
+        Queue<Class<?>> topologySortQueue = new ArrayDeque<>();
+        for(Class<?> clazz : clazzInDegree.keySet()) {
+            if(clazzInDegree.get(clazz) != 0) {
+                continue;
+            }
+            topologySortQueue.add(clazz);
+        }
+
+        return topologySortQueue;
+    }
+
+    private void initializeEachClassForTopologySort(Class<?> clazz) {
+
+        Constructor<?> constructor = Arrays.stream(clazz.getConstructors())
+                .filter(c -> c.isAnnotationPresent(AutoWire.class))
+                .findFirst()
+                .orElseThrow(() -> new ContextInitializeException("No AutoWire constructor found for " + clazz.getName()));
+
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        int inDegreeCount = 0;
+
+        for(Class<?> parameter : parameterTypes) {
+            if(SpringContext.BEAN_MAP.containsKey(parameter)) {
+                continue;
+            }
+            inDegreeCount++;
+
+            //이후 위상정렬 그래프 탐색을 위해 탐색 방향 설정
+            if(!clazzGraph.containsKey(parameter)) {
+                clazzGraph.put(parameter, new HashSet<>());
+            }
+
+            clazzGraph.get(parameter).add(clazz);
+
+        }
+
+        clazzInDegree.put(clazz, inDegreeCount);
+    }
+
+    private int injectConstructorWithTopologySort(Queue<Class<?>> topologySortQueue) {
+        int initializedInstances = 0;
+
+        while(!topologySortQueue.isEmpty()) {
+            Class<?> clazz = topologySortQueue.poll();
+
+            Constructor<?> constructor = Arrays.stream(clazz.getConstructors())
+                    .filter(c -> c.isAnnotationPresent(AutoWire.class))
+                    .findFirst()
+                    .orElseThrow(() -> new ContextInitializeException("No AutoWire constructor found for " + clazz.getName()));
+
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+            Object[] injectInstances = new Object[parameterTypes.length];
+
+            for(int i = 0; i < parameterTypes.length; i++) {
+                injectInstances[i] = Optional.ofNullable(SpringContext.BEAN_MAP.get(parameterTypes[i])).orElseThrow(ContextInitializeException::new);
+            }
+
+            try {
+                Object injectedInstance = constructor.newInstance(injectInstances);
+                SpringContext.BEAN_MAP.put(clazz, injectedInstance);
+                initializedInstances++;
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                throw new ContextInitializeException(e);
+            }
+
+            //다음 대상 확인
+            if(!clazzGraph.containsKey(clazz)) {
+                continue;
+            }
+
+            for(Class<?> nextClazz : clazzGraph.get(clazz)) {
+                Integer nextClazzIndegree = clazzInDegree.computeIfPresent(nextClazz, (k, v) -> v - 1);
+                if(nextClazzIndegree == 0) {
+                    topologySortQueue.add(nextClazz);
+                }
+            }
+        }
+
+        return initializedInstances;
     }
 
     private void injectBySetter() {
