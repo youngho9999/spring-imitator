c1c0e3f177c9f51f8e444897cb475e86f9fac35d
youngho9999 <aplblypt@gmail.com>
2025-05-10 20:21:54 +0900

    [refactor] Spring Context 초기화 로직 메서드 분리

--- src/main/java/live/Main.java
+++ src/main/java/live/Main.java
@@ -11,22 +11,18 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+        initializeSpringContext();
+
+
+
+    }
+
+    private static void initializeSpringContext() {
         // 1. 컴포넌트 스캔
         Set<Class<?>> scannedComponents = ComponentScanner.scanAllComponents();
 
         // 2. 컨텍스트 초기화 및 의존성 주입
         SpringContextInitiator initiator = new SpringContextInitiator();
         initiator.init(scannedComponents);
-
-        //테스트
-        CatController o = (CatController) SpringContext.BEAN_MAP.get(CatController.class);
-        o.cat();
-
-        DogController dog = (DogController) SpringContext.BEAN_MAP.get(DogController.class);
-        dog.bark();
-
-        EagleController eagle = (EagleController) SpringContext.BEAN_MAP.get(EagleController.class);
-        eagle.fly();
-
     }
 }
