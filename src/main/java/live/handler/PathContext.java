8797098b74510a2f1655e98b7a7ded434c4bdfd7
youngho9999 <aplblypt@gmail.com>
2025-05-16 22:50:46 +0900

    [feat] DispatcherServlet v2. PathContext 에서 Value 로 Method 대신 Handler 들고있게 변경

--- src/main/java/live/handler/PathContext.java
+++ src/main/java/live/handler/PathContext.java
@@ -1,10 +1,9 @@
 package live.handler;
 
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PathContext {
 
-    public static final Map<String, Method> PATH_METHOD_MAP = new HashMap<>();
+    public static final Map<String, Handler> PATH_HANDLER_MAP = new HashMap<>();
 }
