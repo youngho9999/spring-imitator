b5c63f36d2e053c2cd284321d0b6a01ab7c8d781
youngho9999 <aplblypt@gmail.com>
2025-06-04 10:19:57 +0900

    [feat] main 메서드에서 webserver 쓰레드 생성 및 실행

--- src/main/java/live/Main.java
+++ src/main/java/live/Main.java
@@ -3,15 +3,41 @@ package live;
 import live.context.ComponentScanner;
 import live.context.SpringContext;
 import live.context.SpringContextInitiator;
+import live.handler.DispatcherServlet;
 import live.handler.PathContextInitiator;
+import live.listener.Acceptor;
+import live.listener.Poller;
 import live.listener.WebServer;
 
+import java.io.IOException;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
         initializeSpringContext();
         intializeDispatcherServlet();
+        initializeWebServer();
+
+
+    }
+
+    private static void initializeWebServer() {
+        final int maxThreads = 2;
+        DispatcherServlet dispatcherServlet = (DispatcherServlet) SpringContext.BEAN_MAP.get(DispatcherServlet.class);
+        ExecutorService executorService = Executors.newFixedThreadPool(maxThreads);
+
+        try {
+            Poller poller = new Poller(executorService, dispatcherServlet);
+            Acceptor acceptor = new Acceptor(poller);
+
+            new Thread(poller).start();
+            new Thread(acceptor).start();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
@@ -28,8 +54,5 @@ public class Main {
     private static void intializeDispatcherServlet() {
         PathContextInitiator initiator = new PathContextInitiator();
         initiator.scanHandlers();
-
-        WebServer webServer = (WebServer) SpringContext.BEAN_MAP.get(WebServer.class);
-        webServer.listenSocket();
     }
 }
